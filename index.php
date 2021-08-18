80a26fb8f88669ddc1b2e813c08fec48841883b1
thoor <javierthoriq6@gmail.com>
2021-08-18 21:32:25 +0700

    mnbhkn list haha

--- index.php
+++ index.php
@@ -11,5 +11,9 @@
         halo hai
     </h1>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum libero exercitationem ratione quisquam, nesciunt cumque deleniti explicabo fuga. Obcaecati atque dolor magni eligendi eaque ipsam natus earum reprehenderit tenetur? Sequi?
+    <h2>
+        <ul>hehe</ul>
+        <ul>haha</ul>
+    </h2>
 </body>
 </html>
